d05e3bbdaa3af1ec8c1e554ebd262c426b857d2b
Joachim Holwech <joachim.holwech@gmail.com>
2019-01-17 07:29:56 +0100

    Bug fixes

--- fix_exif.py
+++ fix_exif.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 import piexif
 
-folder = 'test_images/'
+folder = './'
 
 def get_date(filename):
     date_str = filename.split('-')[1]
@@ -10,8 +10,9 @@ def get_date(filename):
 
 filenames = [fn for fn in os.listdir(folder) if fn.split('.')[-1] == 'jpg']
 l = len(filenames)
+print(l)
 for i, filename in enumerate(filenames):
-    exif_dict = {'0th': { piexif.ImageIFD.DateTime: get_date(filename) }}
+    exif_dict = {'Exif': { piexif.ExifIFD.DateTimeOriginal: get_date(filename) }}
     exif_bytes = piexif.dump(exif_dict)
     piexif.insert(exif_bytes, folder + filename)
     print('{}/{}'.format(i + 1, l))
